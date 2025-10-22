f1dde1855b842d30fb294d33dedbcc818dca9635
Mehedihasan0151 <mehedi2003coc@gmail.com>
2025-10-23 01:08:14 +0600

    route name fix

--- routes/web.php
+++ routes/web.php
@@ -12,21 +12,16 @@ Route::post('/upload', [ResultController::class, 'upload'])->name('upload.pdf');
 Route::get('/', function () {
     return view('search');
 })->name('search');
-Route::get('/juthi', function () {
-    return view('search_juthi');
-})->name('search');
 Route::post('/', [ResultController::class, 'search'])->name('search');
 
+// only for juthi
+Route::get('/juthi', function () {
+    return view('search_juthi'); 
+})->name('juthi.welcome');
+Route::get('/juthi/result', [ResultController::class, 'searchJuthiResult'])->name('juthi.result');
+
+// result show page
 Route::post('/result/{roll}', function ($roll) {
     $result = Result::where('roll', $roll)->first();
     return view('result', compact('result'));
 })->name('result.show');
-
-
-
-Route::get('/juthi', function () {
-    return view('search_juthi'); 
-})->name('juthi.welcome');
-
-// Page that FINDS and SHOWS Juthi's result
-Route::get('/juthi/result', [ResultController::class, 'searchJuthiResult'])->name('juthi.result');
